1cf85e8923cfde05beb8a365abf8ac527ae58abf
Troy Connor <troy0820@users.noreply.github.com>
2016-10-03 14:48:34 -0400

    [package/rules]Added comma-dangle rule for eslint 3.0, updgraded devDep

--- rules/recommended.js
+++ rules/recommended.js
@@ -3,6 +3,10 @@ module.exports = {
   'extends': 'eslint:recommended',
   'rules': {
     // override to allow console.log
-    'no-console': 0
+    'no-console': 0,
+    'comma-dangle' : 'error',
+    'no-unsafe-finally': 'off',
+    'no-native-reassign': 'off',
+    'require-yied': 'error'
   }
 }
