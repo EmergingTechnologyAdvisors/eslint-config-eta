adfb118f8250d7aa08a13af44140020df7b31a38
Troy Connor <troy0820@users.noreply.github.com>
2016-10-05 12:50:37 -0400

    [rules]Put ES6 rules in es6.js and took out no-native-reassign

--- rules/recommended.js
+++ rules/recommended.js
@@ -4,9 +4,6 @@ module.exports = {
   'rules': {
     // override to allow console.log
     'no-console': 0,
-    'comma-dangle' : 'error',
-    'no-unsafe-finally': 'off',
-    'no-native-reassign': 'off',
-    'require-yield': 'error'
+    'comma-dangle' : 'error'
   }
 }
